b73aa33ac68fecd00d39f533d8504a614ad0ed95
Jeffrey Rennie <jeff@rennieglen.com>
2023-09-08 17:11:08 -0700

    Validate form fields before user clicks submit.
    Could this be improved with tag helpers?

--- Exercises/Exercises.End/Pages/09_FormValidation.cshtml.cs
+++ Exercises/Exercises.End/Pages/09_FormValidation.cshtml.cs
@@ -21,8 +21,26 @@ namespace Exercises.Pages
             await Task.Delay(TimeSpan.FromSeconds(1));
             // handle Htmx request
             return Request.IsHtmx()
-                ? Partial("_Form", this)
+                ? Validate(this) ?? Partial("_Form", this)
                 : Page();
+        }
+
+        // If the request query contains ?validate=FieldName, then return
+        // the results of validating that field.
+        static public IActionResult? Validate(PageModel pageModel) {
+            if (pageModel.Request.Query["validate"].Count > 0) {
+                var field = pageModel.Request.Query["validate"][0];
+                var errors = pageModel.ModelState[field].Errors;
+                var id = System.Net.WebUtility.HtmlEncode(field);
+                foreach (var error in errors) {
+                    var text = System.Net.WebUtility.HtmlEncode(error.ErrorMessage);
+                    var scrip = $"<script>document.getElementById('{id}').classList.add('input-validation-error')</script>";
+                    return pageModel.Content(text + scrip, "text/html");
+                }
+                var script = $"<script>document.getElementById('{id}').classList.remove('input-validation-error')</script>";
+                return pageModel.Content(script, "text/html");
+            }
+            return null;
         }
     }
 }
